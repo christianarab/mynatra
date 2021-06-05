852842e0afb9720cb8c1e5dffec3fd19253aa868
christian <rewindforward@gmail.com>
2021-06-05 07:57:06 -0400

    Beginnings
    Mynatura
    --------
    A flexible Sinatra based framework - capable of creating user, blog posts, and comments on the fly.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Mynatra
+
+A flexible Sinatra framework
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+```ruby
+gem 'mynatra'
+```
+
+And then execute:
+
+    $ bundle install
+
+Or install it yourself as:
+
+    $ gem install mynatra
+
+## Usage
+
+TBA
+
+## Development
+
+Work in progress.
---
+++ lib/mynatra/cli.rb
@@ -0,0 +1,13 @@
+require 'thor'
+require 'mynatra'
+
+module Foodie
+  class CLI < Thor
+    # TODO: Add thor commands, desc. mynatra function definitions
+    # Example:
+      # desc "portray [FOOD]", "Determines where a food item is gross or delicious"
+      # def portray(food)
+      #   puts Foodie::Food.portray(food)
+      # end
+  end
+end
