ededd379b37011e47ad7a59e177c488db3b75591
christian <rewindforward@gmail.com>
2021-06-06 17:22:56 -0400

    Issues with template, creating ERB

--- lib/generators/resource.rb
+++ lib/generators/resource.rb
@@ -1,5 +1,4 @@
 require 'thor/group'
-puts __FILE__
 
 module Mynatra
   module Generators
@@ -13,12 +12,24 @@ module Mynatra
         empty_directory(dir)
       end
 
-      def copy_model
-        template("model.txt", "#{dir}/models/#{name}.rb")
+      def create_model
+        template("models/model.txt", "#{dir}/models/#{name}.rb")
+      end
+
+      def create_view
+        template("views/resource/index.txt", "#{dir}/views/#{name}/index.erb")
+      end
+      
+      def create_app
+        template("app.txt", "#{dir}/app.rb")
+      end
+
+      def create_public
+        directory("public", "#{dir}/public", :recursive => true)
       end
 
       def self.source_root
-        File.dirname(__FILE__) + "/models"
+        File.dirname(__FILE__)
       end
     end
   end
