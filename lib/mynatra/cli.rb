9b859357ae601eed0909639bbe1ba915b2864f1f
christian <rewindforward@gmail.com>
2021-07-22 12:30:54 -0400

    Fixes display issues

--- lib/mynatra/cli.rb
+++ lib/mynatra/cli.rb
@@ -6,15 +6,14 @@ require 'generators/resource'
 
 module Mynatra
   class CLI < Thor
-    desc "new [NAME]", "Generate mynatra project"
+    desc "new [NAME]", "Generates new mynatra project"
     def new(name)
       Mynatra::Generators::Scaffolding.start([name.singularize])
     end 
     
     desc "resource [NAME] [ARG1] [ARG2]", "Generates resources (`mynatra resource post title body`)"
     def resource(name, *attributes)
-      puts "The arguments are: #{attributes}"
-      puts "Args is: #{attributes.class}"
+      puts "Generating #{name} model with attributes: #{attributes}"
       Mynatra::Generators::Resource.start([ name.singularize, attributes ])
     end 
   end
